d9d90e73a651dbec12e2c15ebaa5018be0ebda22
Shtey A <a.shtey@krista.ru>
2020-02-21 15:35:07 +0300

    добавил корректную обработку ошибок при старте соединения 2ух TcpSocket'ов

--- pyproto/tcpSocket.py
+++ pyproto/tcpSocket.py
@@ -4,17 +4,23 @@ from .logger import write_info
 
 
 class TcpSocket(TcpWorker):
-    '''сущность которая может подключится к серверу образовав тем самым конекцию,
-        для дальнейшего обмена сообщениями'''
+    """сущность которая может подключится к серверу образовав тем самым конекцию,
+        для дальнейшего обмена сообщениями"""
     def connect(self, disconect_connection_handler=None):
-        ''' Порядок установки соединения '''
+        """ Порядок установки соединения """
         connection = Connection(self)
         try:
             connection.connect((self.ip, self.port))
+            self.start(connection)
         except ConnectionRefusedError as ex:
             write_info('Не удалось, установить соединение, удалённый сервер не доступен')
             return None
-        self.start(connection)
+        except TimeoutError as te:
+            write_info(str(te))
+            return None
+        except TypeError as type_e:
+            write_info(str(type_e))
+            return None
         self.connection_pool.add_connection(connection)
         self._cmd_method_creator(connection)
 
