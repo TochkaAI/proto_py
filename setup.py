5d53f394e3901674402b848e7dc3b668af97d13b
Anton Lebedev <lebedevdes@gmail.com>
2020-02-26 21:20:59 +0300

    Небольшие правки в setyp.py

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
-import setuptools
+from setuptools import setup, find_packages
+
 
 # with open("README.md", "r") as fh:
 #     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="pyproto",
     version="0.0.4",
     description="package with pyproto for PointOfView",
-    packages=setuptools.find_packages(),
+    url="https://github.com/TochkaAI/pyproto",
+    packages=find_packages(),
+    python_requires='>=3.6',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
 )
