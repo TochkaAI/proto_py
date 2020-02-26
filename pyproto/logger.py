87090f60dab8294824883cebe468445855334599
Shtey A <a.shtey@krista.ru>
2020-02-26 16:11:30 +0300

    Добавил возможность изменять имя логфайла pyproto, для этого нужно вызвать функцию import pyproto.pyproto.logger as pyprotologger pyprotologger.reset_config(<LoggerPath>, <LogFileName>)

--- pyproto/logger.py
+++ pyproto/logger.py
@@ -2,14 +2,25 @@
 
 ЗЫ тут я особо пока вообще не парился'''
 import logging
-# from logging.handlers import TimedRotatingFileHandler
 from pyproto.pyproto.config import MAX_LOG_LENGHT
 
-logging.basicConfig(
-    filename='app.log',
-    filemode='a',
-    format='%(asctime)s - %(name)s - %(levelname)s : %(message)s',
-    level=logging.INFO)
+BASE_FOLDER = ''
+LOG_FILE_NAME = 'app.log'
+
+
+def reset_config(folder, file_name):
+    global BASE_FOLDER
+    global LOG_FILE_NAME
+    BASE_FOLDER = folder
+    LOG_FILE_NAME = file_name
+    logging.basicConfig(
+        filename=BASE_FOLDER + LOG_FILE_NAME,
+        filemode='a',
+        format='%(asctime)s - %(name)s - %(levelname)s : %(message)s',
+        level=logging.INFO)
+
+
+reset_config(BASE_FOLDER, LOG_FILE_NAME)
 
 
 def msg_handler(decor_func):
