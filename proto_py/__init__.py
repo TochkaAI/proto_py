9f4b98c02ffa32157bfb3fdd1563df3dae21e1a8
Alexey Shtey <fifallcon@gmail.com>
2020-03-20 10:30:25 +0300

    Импорт Connection'a перенесен в __init__.py Обновлена версия.

--- proto_py/__init__.py
+++ proto_py/__init__.py
@@ -4,4 +4,5 @@
 from .tcpSocket import TcpSocket
 from .tcpServer import TcpServer
 from .message import Message
+from .connection import Connection
 from .baseCommands import BaseCommand, REGISTRY_COMMAND
